c6c416466afdafe998d3f56d7bd75d67c40ddf06
bhavik <kotharibhavik23@gmail.com>
2023-07-15 13:57:49 +0530

    fixed some pugs in set reminder

--- lib/app/services/notification_service.dart
+++ lib/app/services/notification_service.dart
@@ -1,5 +1,4 @@
 import 'dart:developer';
-import 'dart:math' as math;
 
 import 'package:android_alarm_manager_plus/android_alarm_manager_plus.dart';
 import 'package:firebase_messaging/firebase_messaging.dart';
